170cbc4d0aa4f27667c15674958669bc3fcb457d
Mason Phillips <mjphillips2000@gmail.com>
2020-12-30 03:17:54 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -1,7 +1,15 @@
+
 class Application
-  
+
   def call(env)
     resp = Rack::Response.new
-    
-    
-    if time
+    now = Time.now
+    if now.hour >=12
+      resp.write "Good Afternoon"
+    else
+      resp.write "Good Morning"
+    end
+    resp.finish
+  end
+
+end
